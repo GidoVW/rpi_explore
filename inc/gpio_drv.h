02332ce1f60c082edfa76d5c413089ab1ec14e7c
Gido van Wijk <gido_vanwijk@hotmail.com>
2018-03-25 22:10:52 +0200

    Update inc and src

--- inc/gpio_drv.h
+++ inc/gpio_drv.h
@@ -3,17 +3,19 @@
 
 #include <stdint.h>
 
-#define BCM2835_PERIPH_BASE 0x20000000
+#define BCM2835_PERIPH_BASE 0x7e000000
 #define GPIO_OFFSET         0x00200000
 #define GPIO_BASE           (BCM2835_PERIPH_BASE + GPIO_OFFSET)
-#define GPSET0              0x0000001C
+#define GPSET0              7
+#define GPCLR0              10
+#define GPLEV0              13
 
 #define BLOCK_SIZE          (4*1024)
 
 struct BcmPeriph {
   uint32_t          addr_ptr;
   int               mem_fd;
-  char              *map;
+  void              *map;
   volatile uint32_t *addr;
 };
 
--- src/gpio_drv.c
+++ src/gpio_drv.c
@@ -6,6 +6,8 @@
  *
  * @changes
  * 12/02/18 - First version
+ * 22/02/18 - Added set/clear registers
+ * 25/03/18 - Further improvements and debugging
  **/
 
 #include "gpio_drv.h"
@@ -19,7 +21,7 @@
 
 #include <unistd.h>
 
-struct BcmPeriph gpio = {GPIO_BASE};
+struct BcmPeriph gpio = {GPIO_BASE, 0, NULL, NULL};
 
 int MapPeripheral(struct BcmPeriph *p)
 {
@@ -30,7 +32,7 @@ int MapPeripheral(struct BcmPeriph *p)
     perror("open");
   } else {
     printf("File Descriptor: %d\n", p->mem_fd); 
-    p->map = (char*)mmap(NULL,
+    p->map = mmap(NULL,
                   BLOCK_SIZE,
                   PROT_READ|PROT_WRITE,
                   MAP_SHARED,
@@ -39,7 +41,7 @@ int MapPeripheral(struct BcmPeriph *p)
 
     if (p->map != MAP_FAILED) {
       p->addr = (volatile uint32_t *)p->map;
-      printf("Mapped address: %d\n", *(p->addr));
+      printf("Mapped address: %d, value %d\n", p->addr, *(p->addr));
       ret = 1;
     } else {
       printf("MAP FAILED.\n");
@@ -62,6 +64,8 @@ int InitGpioOutput(int gpio_nb)
   int ret = 0;
 
   if(MapPeripheral(&gpio)) {
+    printf("Current config: %d\n", *(gpio.addr));
+    *(gpio.addr) = 0;
     *(gpio.addr) = 1 << 12;
     UnmapPeripheral(&gpio);
     ret = 1;
@@ -74,9 +78,18 @@ int InitGpioOutput(int gpio_nb)
 int SetGpioOutput(int gpio_nb)
 {
   int ret = 0;
+  int current_val = 0;
 
   if(MapPeripheral(&gpio)) {
-    *(gpio.addr+GPSET0) = gpio_nb << 4;
+    current_val = *(gpio.addr+13);
+    printf("Current value of pin is:  %d\n", current_val);
+    if (gpio_nb == 1) {
+      printf("Setting bit, current val: %d\n", *(gpio.addr+7));
+      *(gpio.addr+7) = (1 << 4);
+    } else if (gpio_nb == 0) {
+      printf("Clearing bit, current val: %d\n", *(gpio.addr+10));
+      *(gpio.addr+10) = (1 << 4);
+    }
     UnmapPeripheral(&gpio);
     ret = 1; 
   } else {
