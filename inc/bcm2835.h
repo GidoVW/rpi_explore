7d13a0a093a817f5d9ccbddb99f18f406e089193
Gido van Wijk <gwijkvan@jaguarlandrover.com>
2018-06-13 03:56:10 -0700

    Add GPIO register definitions

--- inc/bcm2835.h
+++ inc/bcm2835.h
@@ -22,6 +22,87 @@ typedef struct {
   volatile uint32_t *virt_addr;
 } __bcm_periph_s;
 
+
+/************************************
+ *          GPIO REGISTERS          *
+ ************************************/
+
+/*
+
+	EXAMPLE:
+
+	Let's say you want to access GPFSEL1.
+	First, as there is an OS layer, you need to mmap the
+	memory region you want to access. This is done through API:
+
+			int _MapPeripheral (__bcm_periph_s *p);
+
+	you have to provide:	
+
+			._bcm_hw_addr 	= GPIO_BASE,
+			._mem_fd      	= open(/dev/gpiomem, ...),
+			
+	This will in turn give you 
+
+			._map 		: void pointer to mapped area
+			.virt_addr	: non-void pointer to mapped area
+
+	Using the pointer to the mapped area, you can translate
+	that into the struct defined below to easily access
+	byte fields:
+
+			__bcm_periph_s gpio_periph;
+			GPIO *mygpio;
+
+			mygpio = (GPIO *)gpio_periph._map;
+
+	Finally,
+
+			mygpio.GPFSEL1.FIELD.FSEL4 = 0b001;
+
+*/
+
+typedef union {
+	uint32_t REG;
+	struct {
+		uint32_t FSEL0:3;
+		uint32_t FSEL1:3;
+		uint32_t FSEL2:3;
+		uint32_t FSEL3:3;
+		uint32_t FSEL4:3;
+		uint32_t FSEL5:3;
+		uint32_t FSEL6:3;
+		uint32_t FSEL7:3;
+		uint32_t FSEL8:3;
+		uint32_t FSEL9:3;
+		uint32_t :2;       /* reserved */
+	} FIELD;
+} GPFSEL0;
+
+typedef union {
+	uint32_t REG;
+	struct {
+		uint32_t FSEL10:3;
+		uint32_t FSEL11:3;
+		uint32_t FSEL12:3;
+		uint32_t FSEL13:3;
+		uint32_t FSEL14:3;
+		uint32_t FSEL15:3;
+		uint32_t FSEL16:3;
+		uint32_t FSEL17:3;
+		uint32_t FSEL18:3;
+		uint32_t FSEL19:3;
+		uint32_t :2;       /* reserved */
+	} FIELD;
+} GPFSEL1;
+
+typedef struct {
+	
+	GPFSEL0 gpfsel0_reg;
+	GPFSEL1 gpfsel1_reg;
+
+} GPIO;
+
 extern int  _MapPeripheral  (__bcm_periph_s *p);
 extern void _UnmapPeripheral(__bcm_periph_s *p);
 
